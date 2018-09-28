e2e90b76c3ef16ccaf9a50701b9e6677abe3f4af
feng <edu_feng@163.com>
2018-09-28 16:13:36 +0800

    chore: add __version__.py

---
+++ dynalist/__version__.py
@@ -0,0 +1,8 @@
+__version__ = '0.2.3'
+__name__ = 'dynalist'
+__description__ = 'Unofficial Dynalist API Written in Python'
+__url__ = 'https://github.com/georgefeng/dynalist'
+__author__ = 'feng'
+__authoremail__ = 'edu_feng@163.com'
+__license__ = 'The MIT License (MIT)'
+__copyright__ = 'Copyright 2018 feng'
--- setup.py
+++ setup.py
@@ -2,26 +2,31 @@
 # coding=utf-8
 
 from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(here, 'dynalist', '__version__.py'),
+          mode='r') as f:
+    exec(f.read(), about)
+
+with open('README.md', mode='r') as f:
+    readme = f.read()
 
 setup(
-    name='dynalist',
-    version='0.1.2',
-    description=(
-        'Unofficial Dynalist API Written in Python'
-    ),
+    name=about['__name__'],
+    description=about['__description__'],
     long_description=open('README.md').read(),
-    long_description_content_type="text/markdown",
-    author='feng',
-    author_email='edu_feng@163.com',
-    maintainer='feng',
-    maintainer_email='edu_feng@163.com',
-    license='MIT License',
-    packages=find_packages(),
-    platforms=["all"],
-    url='https://github.com/georgefeng/dynalist',
-    install_requires=[
-        'requests',
-    ],
+    long_description_content_type='text/markdown',
+    author=about['__author__'],
+    author_email=about['__authoremail__'],
+    url=about['__url__'],
+    version=about['__version__'],
+    packages=find_packages('./dynalist'),
+    install_requires=['requests>=2.18.3'],
+    keywords=['dynalist', 'api'],
+    license=about['__license__'],
     classifiers=[
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
@@ -34,3 +39,38 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 )
+
+
+# from setuptools import setup, find_packages
+
+# setup(
+#     name='dynalist',
+#     version='0.2.0',
+#     description=(
+#         'Unofficial Dynalist API Written in Python'
+#     ),
+#     long_description=open('README.md').read(),
+#     long_description_content_type="text/markdown",
+#     author='feng',
+#     author_email='edu_feng@163.com',
+#     maintainer='feng',
+#     maintainer_email='edu_feng@163.com',
+#     license='MIT License',
+#     packages=find_packages(),
+#     platforms=["all"],
+#     url='https://github.com/georgefeng/dynalist',
+#     install_requires=[
+#         'requests',
+#     ],
+#     classifiers=[
+#         'Operating System :: OS Independent',
+#         'Intended Audience :: Developers',
+#         'License :: OSI Approved :: MIT License',
+#         'Programming Language :: Python',
+#         'Programming Language :: Python :: Implementation',
+#         'Programming Language :: Python :: 3',
+#         'Programming Language :: Python :: 3.4',
+#         'Programming Language :: Python :: 3.5',
+#         'Programming Language :: Python :: 3.6',
+#     ],
+# )
